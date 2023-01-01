a1997b7c0a2fde256e1438adaabe3c5e395ce4db
duechayapolgmit <g00388741@atu.ie>
2023-01-01 15:33:42 +0000

    logout

--- mcc-stats/src/components/backend/BackEnd_Nav.js
+++ mcc-stats/src/components/backend/BackEnd_Nav.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Navbar } from 'react-bulma-components';
 
 export default class BackEnd_Nav extends React.Component {
+    logOut(){
+        localStorage.removeItem('token')
+    }
+
     render(){
         return (
             <div>
@@ -10,6 +14,7 @@ export default class BackEnd_Nav extends React.Component {
                     <Navbar.Item backgroundColor="link" textColor="white" href="/admin/add">Add Player</Navbar.Item>
                     <Navbar.Item backgroundColor="link" textColor="white" href="/admin/edit">Edit Player</Navbar.Item>
                     <Navbar.Item backgroundColor="link" textColor="white" href="/admin/delete">Delete Player</Navbar.Item>
+                    <Navbar.Item backgroundColor="mcc" textColor="white" href="/mcc" onClick={this.logOut}>Logout</Navbar.Item>
                     <Navbar.Container align="right">
                         <Navbar.Item backgroundColor="mcc" textColor="white" href="/">Home</Navbar.Item>
                     </Navbar.Container>
